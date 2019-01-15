16c7725aa93831fe0af12685a1381dc91f81e45e
Kiriko <kiriko626@gmail.com>
2019-01-15 20:14:34 +0500

    First commit

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+import './search-panel.css';
+
+export default class SearchPanel extends Component {
+  
+  onSearch = (e) =>{
+    this.props.onSearch(e.target.value.toLowerCase())
+    }
+  
+  render(){
+    return (
+      <input type="text"
+                className="form-control search-input"
+                placeholder="type to search" onChange={this.onSearch}/>
+    );
+  }
+};
+
+
