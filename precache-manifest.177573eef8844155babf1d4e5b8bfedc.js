98c1689d8f1b2baa816693605799ef8040ecff03
Kiriko626 <kiriko626@gmail.com>
2019-02-26 21:36:28 +0500

    init for build

---
+++ precache-manifest.177573eef8844155babf1d4e5b8bfedc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "491db43aeae0292dc73b",
+    "url": "/static/js/main.491db43a.chunk.js"
+  },
+  {
+    "revision": "c26583f2bc635d6b5c6e",
+    "url": "/static/js/1.c26583f2.chunk.js"
+  },
+  {
+    "revision": "491db43aeae0292dc73b",
+    "url": "/static/css/main.889a2ac3.chunk.css"
+  },
+  {
+    "revision": "74c01f57de33a37b86c8475ee8bcef4c",
+    "url": "/index.html"
+  }
+];
